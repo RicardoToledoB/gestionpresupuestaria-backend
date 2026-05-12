a9d9616b09a0a356d2c9b372cde31d5e955d04a6
Ricardo Toledo <ricardotoledo@Ricardos-MacBook-Pro.local>
2026-05-11 22:39:21 -0300

    kjaskdjakjsd

--- src/main/java/cl/dssm/presupuesto/repository/ExcelImportErrorRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.dssm.presupuesto.repository;
-
-import cl.dssm.presupuesto.entity.ExcelImportError;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ExcelImportErrorRepository extends JpaRepository<ExcelImportError, Long> {
-    List<ExcelImportError> findByExcelImportId(Long importId);
-}
