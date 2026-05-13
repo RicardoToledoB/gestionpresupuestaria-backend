22573b5c8861c1ee247d300e2e9a9e6bffbc9ea3
Ricardo Toledo <ricardotoledo@Ricardos-MacBook-Pro.local>
2026-05-13 00:28:22 -0300

    lkasdlkalksd

---
+++ src/main/java/cl/dssm/presupuesto/enums/PurchaseOrderStatus.java
@@ -0,0 +1,14 @@
+package cl.dssm.presupuesto.enums;
+
+public enum PurchaseOrderStatus {
+    SIN_ESTADO,
+    ANULADA,
+    EJECUTADA,
+    PENDIENTE,
+    EMITIDA,
+    ENVIADA,
+    ACEPTADA,
+    RECEPCIONADA,
+    CANCELADA,
+    CERRADA
+}
