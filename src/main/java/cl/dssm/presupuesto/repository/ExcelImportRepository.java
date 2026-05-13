39ef805271244643e74cad22f08957b1911bc715
Ricardo Toledo <ricardotoledo@Ricardos-MacBook-Pro.local>
2026-05-13 15:44:12 -0300

    lkasdlkalksd

--- src/main/java/cl/dssm/presupuesto/repository/ExcelImportRepository.java
+++
@@ -1,6 +0,0 @@
-package cl.dssm.presupuesto.repository;
-
-import cl.dssm.presupuesto.entity.ExcelImport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExcelImportRepository extends JpaRepository<ExcelImport, Long> {}
