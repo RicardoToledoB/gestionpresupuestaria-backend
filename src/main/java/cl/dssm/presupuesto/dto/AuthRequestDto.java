3c7dad279adeeb60c6670bcadfecb500e0c22221
Ricardo Toledo <ricardotoledo@Ricardos-MacBook-Pro.local>
2026-05-13 15:55:28 -0300

    lkasdlkalksd

--- src/main/java/cl/dssm/presupuesto/dto/AuthRequestDto.java
+++
@@ -1,8 +0,0 @@
-package cl.dssm.presupuesto.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record AuthRequestDto(
-        @NotBlank String username,
-        @NotBlank String password
-) {}
