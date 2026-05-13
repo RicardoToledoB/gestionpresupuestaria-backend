794dbfed951b0ebbc5c902d690fd137467d2f15f
Ricardo Toledo <ricardotoledo@Ricardos-MacBook-Pro.local>
2026-05-13 15:08:18 -0300

    lkasdlkalksd

--- src/main/java/cl/dssm/presupuesto/repository/BudgetMovementRepository.java
+++
@@ -1,6 +0,0 @@
-package cl.dssm.presupuesto.repository;
-
-import cl.dssm.presupuesto.entity.BudgetMovement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BudgetMovementRepository extends JpaRepository<BudgetMovement, Long> {}
