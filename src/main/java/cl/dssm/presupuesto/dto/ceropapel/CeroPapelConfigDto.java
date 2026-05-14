18c69af739297adde283e852f4a42111f9428e42
Ricardo Toledo <ricardotoledo@Ricardos-MacBook-Pro.local>
2026-05-14 11:59:43 -0300

    lkasdlkalksd

--- src/main/java/cl/dssm/presupuesto/dto/ceropapel/CeroPapelConfigDto.java
+++
@@ -1,8 +0,0 @@
-package cl.dssm.presupuesto.dto.ceropapel;
-
-public record CeroPapelConfigDto(
-        String baseUrl,
-        String authPath,
-        String purchaseRequestsPath,
-        boolean credentialsConfigured
-) {}
